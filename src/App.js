b0aeff6db5844daa9579bbf0e58394a34044eb0c
Eriks Kisko <erixk2807@gmail.com>
2022-02-21 13:01:18 +0000

    Create a static page with dark/light toggle button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import {useState} from "react"
 import './App.css';
+import Nav from './components/Nav';
+import MainContent from './components/MainContent';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [darkMode, setDarkMode] = useState(false)
+  function toggleDarkMode() {
+      setDarkMode((prevState) => !prevState)
+  }
+
+  return(
+    <div className='App'>
+    <Nav 
+      toggleDarkMode={toggleDarkMode}
+      darkMode={darkMode}
+    />
+      <MainContent 
+      darkMode={darkMode}
+      />
     </div>
-  );
+  )
 }
 
 export default App;
